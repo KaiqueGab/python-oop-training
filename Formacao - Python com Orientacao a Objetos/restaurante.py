2fd030acf8152dc892c2264e4ad16d012f03ea52
KaiqueGab <kaiquegabriel973@gmail.com>
2025-07-01 21:27:37 -0300

    Criando um novo metodo __str__ para a classe Restaurante

--- Formacao - Python com Orientacao a Objetos/restaurante.py
+++ Formacao - Python com Orientacao a Objetos/restaurante.py
@@ -1,13 +1,17 @@
 class Restaurante:
-    def __init__(self, nome, categoria):
+    def __init__(self, nome, categoria): #Método de inicializar o Construtor
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
 
-restaurante_praca = Restaurante('praça', 'pizzaria')
+    def __str__(self): #Método de pegar a localização do objetivo na memoria e trazer o valor em formato String
+        return f'{self.nome} | {self.categoria}'
+
+#Instanciando os Objetos
+restaurante_praca = Restaurante('Praça', 'Pizzaria')
 restaurante_pizza = Restaurante('PizzaExpress', 'Italiana')
 
 restaurantes = [restaurante_pizza, restaurante_praca]
 
-print(vars(restaurante_praca))
-print(vars(restaurante_pizza))
+print(restaurante_praca)
+print(restaurante_pizza)
